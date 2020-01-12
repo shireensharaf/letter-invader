fe741d6b2d2fb4cb924dfa210744fe76d70466f1
shireensharaf <shirenmsharaf@gmail.com>
2020-01-12 21:30:16 +0530

    letter position - test green

---
+++ letter_invader.py
@@ -0,0 +1,2 @@
+def letter_position(letter, max_ht):
+    return {'a' : [20, 100]}
--- test_invader.py
+++ test_invader.py
@@ -1,4 +1,4 @@
 import letter_invader
 
 def test_letter_and_position():
-    assert letter_invader.letter_position([{}, 100]) == {'a' : [20, 100]}
+    assert letter_invader.letter_position({}, 100) == {'a' : [20, 100]}
