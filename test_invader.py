668900d478d7b88d65187191db2dcb3a04f89b91
shireensharaf <shirenmsharaf@gmail.com>
2020-01-12 22:17:23 +0530

    letter position - test refactor

--- test_invader.py
+++ test_invader.py
@@ -1,4 +1,10 @@
 import letter_invader
 
-def test_letter_and_position():
-    assert letter_invader.letter_position({}, 100) == {'a' : [20, 100]}
+def test_letter_and_position_initial():
+    assert letter_invader.letter_position({}, ['a', 20], 100) == {'a' : [20, 100]}
+
+def test_letter_and_position_update_position_no_input():
+    assert letter_invader.letter_position({'a' : [20, 100]}, [], 100) == {'a' : [20, 99]}
+
+def test_letter_and_position_update_position_with_input():
+    assert letter_invader.letter_position({'a' : [20, 100]}, ['g', 45], 100) == {'a' : [20, 99], 'g' : [45, 100]}
