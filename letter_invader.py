23a43e043c3f38a09f8271918a6105af200e66d5
shireensharaf <shirenmsharaf@gmail.com>
2020-01-12 23:07:38 +0530

    score process - test red

--- letter_invader.py
+++ letter_invader.py
@@ -1,9 +1,11 @@
 max_ht = 100
 
 def letter_position(letter_position, new_letter, max_ht):
+    "letter and its position for viewport and update existing position"
     for letter, position in letter_position.items():
         position[1] -= 1
         letter_position[letter] = position
     if(new_letter):
         letter_position[new_letter[0]] = [new_letter[1], max_ht]
     return letter_position
+
--- test_invader.py
+++ test_invader.py
@@ -3,8 +3,14 @@ import letter_invader
 def test_letter_and_position_initial():
     assert letter_invader.letter_position({}, ['a', 20], 100) == {'a' : [20, 100]}
 
-def test_letter_and_position_update_position_no_input():
+def test_letter_and_update_position_no_new():
     assert letter_invader.letter_position({'a' : [20, 100]}, [], 100) == {'a' : [20, 99]}
 
-def test_letter_and_position_update_position_with_input():
+def test_letter_and_update_position_insert_new_letter():
     assert letter_invader.letter_position({'a' : [20, 100]}, ['g', 45], 100) == {'a' : [20, 99], 'g' : [45, 100]}
+
+
+#process
+
+def test_process_no_input():
+    assert letter_invader.process(['', {'a' : [20, 99], 'g' : [45, 100]}, 0, 5]) == [{'a' : [20, 99], 'g' : [45, 100]}, 0, 5]
