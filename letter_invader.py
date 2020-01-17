c389d91677020c2dcb51f0869abb32dfbc1d17dd
shireensharaf <shirenmsharaf@gmail.com>
2020-01-17 15:48:41 +0530

    score process

--- letter_invader.py
+++ letter_invader.py
@@ -1,7 +1,7 @@
 max_ht = 100
 
 def letter_position(letter_position, new_letter, max_ht):
-    "letter and its position for viewport and update existing position"
+    "new letter and its position for viewport and update existing position"
     for letter, position in letter_position.items():
         position[1] -= 1
         letter_position[letter] = position
@@ -12,7 +12,17 @@ def letter_position(letter_position, new_letter, max_ht):
 
 def process(user_input, letter_position, score, life):
     "Update score & remaining life"
+    del_letter = []
     if(user_input in letter_position.keys()):
         del letter_position[user_input]
         score += 1
+    for letter, position in letter_position.items():
+        if(position[1] == 0):
+            del_letter.append(letter)
+            life -= 1
+    if(del_letter):
+        for i in del_letter:
+            del letter_position[i]     
     return [letter_position, score, life]
+
+
--- test_invader.py
+++ test_invader.py
@@ -17,3 +17,6 @@ def test_process_no_input():
 
 def test_process_correct_input():
     assert letter_invader.process('a', {'a' : [20, 99], 'g' : [45, 100]}, 0, 5) == [{'g' : [45, 100]}, 1, 5]
+
+def test_process_reduce_life():
+    assert letter_invader.process('', {'a' : [20, 99], 'g' : [45, 0]}, 1, 5) == [{'a' : [20, 99]}, 1, 4]
