884ccc63fe57efaf5647473008d5061dbb896847
shireensharaf <shirenmsharaf@gmail.com>
2020-01-20 16:50:46 +0530

    time delay

--- letter_invader.py
+++ letter_invader.py
@@ -1,28 +1,75 @@
-max_ht = 100
+import string
+import random
+import time
+import curses
 
-def letter_position(letter_position, new_letter, max_ht):
+def move_letter(letter_position):
     "new letter and its position for viewport and update existing position"
     for letter, position in letter_position.items():
-        position[1] -= 1
+        position[1] += 1
         letter_position[letter] = position
-    if(new_letter):
-        letter_position[new_letter[0]] = [new_letter[1], max_ht]
+    
     return letter_position
 
+def create_letter(letter_position, new_letter, new_position):
+    "create new letter with its position in viewport"
+    if new_letter not in letter_position.keys():
+        letter_position[new_letter] = [new_position, 0]
+    return letter_position
 
-def process(user_input, letter_position, score, life):
+def process(user_input, letter_position, score, life, max_ht):
     "Update score & remaining life"
     del_letter = []
-    if(user_input in letter_position.keys()):
+    if user_input in letter_position.keys():
         del letter_position[user_input]
         score += 1
     for letter, position in letter_position.items():
-        if(position[1] == 0):
+        if position[1] == max_ht:
             del_letter.append(letter)
             life -= 1
-    if(del_letter):
+    if del_letter:
         for i in del_letter:
-            del letter_position[i]     
-    return [letter_position, score, life]
+            del letter_position[i]
+    if life==0:
+        return (letter_position, score, life, True)
+    return (letter_position, score, life, False)
 
+def delay(score, delay):
+    time_delay = 1
+    if score >= 10:
+        time_delay = 1 - int(score/10) * delay
+        if time_delay < .1:
+            return .1
+    return time_delay
 
+def main(window):
+    letters_and_positions = {}
+    score = 0
+    life = 5
+    gameover = False
+    height = window.getmaxyx()[0]-1
+    width = window.getmaxyx()[1]-1
+    curses.curs_set(0)
+    window.nodelay(True)
+    while(not gameover):
+        letters_and_positions = create_letter(letters_and_positions, random.choice(string.ascii_lowercase), random.choice(range(0,width)))
+        letters_and_positions = move_letter(letters_and_positions)
+        try:
+            user_input = window.getkey()
+        except:
+            user_input = ''
+        letter_position, score, life, gameover = process(user_input, letters_and_positions, score, life,height)
+        for letter, position in letter_position.items():
+            window.addstr(position[1], position[0], letter)
+            
+        window.addstr(0, 0,'Score : {}'.format(score) )
+        window.addstr(0, width - 10,'Life : {}'.format(life) )
+        window.refresh()
+        time.sleep(delay(score, 1))
+        window.clear()
+    window.addstr(int(height/2), int((width/2)-4), 'Game Over')
+    window.refresh()
+    time.sleep(3)
+
+if __name__ == '__main__':
+    curses.wrapper(main)
--- test_invader.py
+++ test_invader.py
@@ -1,22 +1,63 @@
 import letter_invader
 
-def test_letter_and_position_initial():
-    assert letter_invader.letter_position({}, ['a', 20], 100) == {'a' : [20, 100]}
+def test_letter_and_update_position():
+    assert letter_invader.move_letter({'a' : [20, 0]}) == {'a' : [20, 1]}
 
-def test_letter_and_update_position_no_new():
-    assert letter_invader.letter_position({'a' : [20, 100]}, [], 100) == {'a' : [20, 99]}
-
-def test_letter_and_update_position_insert_new_letter():
-    assert letter_invader.letter_position({'a' : [20, 100]}, ['g', 45], 100) == {'a' : [20, 99], 'g' : [45, 100]}
+def test_letter_and_update_position_multiple():
+    assert letter_invader.move_letter({'a' : [20, 0], 'g': [45, 10]}) == {'a' : [20, 1], 'g' : [45, 11]}
 
 
+#create new letter position pair
+def test_letter_insert_new_letter():
+    assert letter_invader.create_letter({'a' : [20, 0]}, 'g', 45) == {'a' : [20, 0], 'g' : [45, 0]}
+
+def test_letter_new_letter_repeat():
+    assert letter_invader.create_letter({'a' : [20, 10]}, 'a', 45) == {'a' : [20, 10]}
+
+#time delay
+
+def test_delay_first_state():
+    assert letter_invader.delay(2, .1) == 1
+
+def test_delay_reduce_delay():
+    assert letter_invader.delay(10, .1) == .9
+
+def test_delay_no_change():
+    assert letter_invader.delay(11, .1) == .9
+
+def test_delay_second():
+    assert letter_invader.delay(23, .1) == .8
+def test_minimum_delay():
+    assert letter_invader.delay(100, .1) == .1
+
+   
+    
 #process
 
 def test_process_no_input():
-    assert letter_invader.process('', {'a' : [20, 99], 'g' : [45, 100]}, 0, 5) == [{'a' : [20, 99], 'g' : [45, 100]}, 0, 5]
+    letter_and_position, score, life, game_over =  letter_invader.process('', {'a' : [20, 1], 'g' : [45, 0]}, 0, 5, 100)
+    assert letter_and_position == {'a' : [20, 1], 'g' : [45, 0]}
+    assert score == 0
+    assert life == 5
 
 def test_process_correct_input():
-    assert letter_invader.process('a', {'a' : [20, 99], 'g' : [45, 100]}, 0, 5) == [{'g' : [45, 100]}, 1, 5]
+    letter_and_position, score, life, game_over = letter_invader.process('a', {'a' : [20, 1], 'g' : [45, 0]}, 0, 5, 100)
+    assert letter_and_position == {'g' : [45, 0]}
+    assert score ==  1
+    assert life == 5
 
 def test_process_reduce_life():
-    assert letter_invader.process('', {'a' : [20, 99], 'g' : [45, 0]}, 1, 5) == [{'a' : [20, 99]}, 1, 4]
+    letter_and_position, score, life, game_over = letter_invader.process('', {'a' : [20, 1], 'g' : [45, 100]}, 1, 5, 100)
+    assert letter_and_position == {'a' : [20, 1]}
+    assert score ==  1
+    assert life == 4
+    assert game_over == False
+    
+    
+def test_process_gameover():
+    letter_and_position, score, life, game_over = letter_invader.process('', {'a' : [20, 1], 'g' : [45, 100]}, 1, 1, 100)
+    assert letter_and_position == {'a' : [20, 1]}
+    assert score ==  1
+    assert life == 0
+    assert game_over == True
+
